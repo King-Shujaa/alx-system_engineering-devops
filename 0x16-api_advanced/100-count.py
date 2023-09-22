23222e82bc15112255c35a2881a2fb29bd95848c
King-Shujaa <davidmburu101@gmail.com>
2023-09-22 05:33:34 +0300

    Update 100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,31 +1,43 @@
 #!/usr/bin/python3
-import praw
-import re
-def count_words(subreddit, word_list):
-    # Convert word_list to lowercase and split it into a list of words
-    word_list = [word.lower() for word in word_list.split()]
-    # Create a praw instance
-    reddit = praw.Reddit(client_id='', client_secret='', user_agent='')
-    # Get the hot posts from the subreddit
-    posts = reddit.subreddit(subreddit).hot(limit=100)
-    # Create a dictionary to store the word counts
-    word_counts = {}
-    # Recursively call the function on each post
-    for post in posts:
-        count_words_in_post(post, word_list, word_counts)
-    # Sort the word counts in descending order
-    sorted_word_counts = sorted(word_counts.items(), key=lambda x: (-x[1], x[0]))
-    # Print the word counts
-    for word, count in sorted_word_counts:
-        print(f"{word}: {count}")
-def count_words_in_post(post, word_list, word_counts):
-    # Get the title of the post
-    title = post.title.lower()
-    # Remove any words that are not in the word list
-    words = [word for word in title.split() if word in word_list]
-    # Count the words
-    for word in words:
-        word_counts[word] = word_counts.get(word, 0) + 1
-    # Recursively call the function on the comments
-    for comment in post.comments:
-        count_words_in_post(comment, word_list, word_counts)
+"""Contains the count_words function"""
+import requests
+
+
+def count_words(subreddit, word_list, found_list=[], after=None):
+    '''Prints counts of given words found in hot posts of a given subreddit.
+
+    Args:
+        subreddit (str): The subreddit to search.
+        word_list (list): The list of words to search for in post titles.
+        found_list (obj): Key/value pairs of words/counts.
+        after (str): The parameter for the next page of the API results.
+    '''
+    user_agent = {'User-agent': 'test45'}
+    posts = requests.get('http://www.reddit.com/r/{}/hot.json?after={}'
+                         .format(subreddit, after), headers=user_agent)
+    if after is None:
+        word_list = [word.lower() for word in word_list]
+
+    if posts.status_code == 200:
+        posts = posts.json()['data']
+        aft = posts['after']
+        posts = posts['children']
+        for post in posts:
+            title = post['data']['title'].lower()
+            for word in title.split(' '):
+                if word in word_list:
+                    found_list.append(word)
+        if aft is not None:
+            count_words(subreddit, word_list, found_list, aft)
+        else:
+            result = {}
+            for word in found_list:
+                if word.lower() in result.keys():
+                    result[word.lower()] += 1
+                else:
+                    result[word.lower()] = 1
+            for key, value in sorted(result.items(), key=lambda item: item[1],
+                                     reverse=True):
+                print('{}: {}'.format(key, value))
+    else:
+        return
