e8053224cf05865dcc5c240dd0d50dc3881f3804
King-Shujaa <113645818+King-Shujaa@users.noreply.github.com>
2023-09-22 05:31:52 +0300

    Update 100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,46 +1,31 @@
 #!/usr/bin/python3
-""" Module for a function that queries the Reddit API recursively."""
-
-import requests
-
-def count_words(subreddit, word_list, after=None, word_dict=None):
-    """ A function that queries the Reddit API parses the title of
-    all hot articles, and prints a sorted count of given keywords
-    (case-insensitive, delimited by spaces.
-    Javascript should count as javascript, but java should not).
-    If no posts match or the subreddit is invalid, it prints nothing.
-    """
-
-    if word_dict is None:
-        word_dict = {word.lower(): 0 for word in word_list}
-
-    if after is None:
-        word_dict_sorted = sorted(word_dict.items(), key=lambda x: (-x[1], x[0]))
-        for word, count in word_dict_sorted:
-            if count:
-                print('{}: {}'.format(word, count))
-        return
-
-    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
-    headers = {'User-Agent': 'redquery'}
-    params = {'limit': 100, 'after': after}
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
-
-    if response.status_code != 200:
-        return
-
-    try:
-        data = response.json()['data']
-        hot = data['children']
-        aft = data['after']
-        
-        for post in hot:
-            title = post['data']['title'].lower()
-            
-            for word in word_dict:
-                word_dict[word] += title.count(word)
-    
-    except Exception:
-        return
-
-    count_words(subreddit, word_list, aft, word_dict)
+import praw
+import re
+def count_words(subreddit, word_list):
+    # Convert word_list to lowercase and split it into a list of words
+    word_list = [word.lower() for word in word_list.split()]
+    # Create a praw instance
+    reddit = praw.Reddit(client_id='', client_secret='', user_agent='')
+    # Get the hot posts from the subreddit
+    posts = reddit.subreddit(subreddit).hot(limit=100)
+    # Create a dictionary to store the word counts
+    word_counts = {}
+    # Recursively call the function on each post
+    for post in posts:
+        count_words_in_post(post, word_list, word_counts)
+    # Sort the word counts in descending order
+    sorted_word_counts = sorted(word_counts.items(), key=lambda x: (-x[1], x[0]))
+    # Print the word counts
+    for word, count in sorted_word_counts:
+        print(f"{word}: {count}")
+def count_words_in_post(post, word_list, word_counts):
+    # Get the title of the post
+    title = post.title.lower()
+    # Remove any words that are not in the word list
+    words = [word for word in title.split() if word in word_list]
+    # Count the words
+    for word in words:
+        word_counts[word] = word_counts.get(word, 0) + 1
+    # Recursively call the function on the comments
+    for comment in post.comments:
+        count_words_in_post(comment, word_list, word_counts)
